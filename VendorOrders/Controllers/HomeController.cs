b5e094f0b778c46c6ac86251791dd6b3d1828d0c
Tyler Adams <skipmarcel@gmail.com>
2023-05-07 13:05:43 -0700

    add initial cshtml files

---
+++ VendorOrders/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorOrders.Models;
+
+namespace VendorOrders.Controllers
+{
+  public class HomeController : Controller
+  {
+
+[HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
+  }
+}
