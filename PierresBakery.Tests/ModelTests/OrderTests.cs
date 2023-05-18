1dc975f81f16dabaea77ad38aeff78d6713e20fe
Tyler Adams <skipmarcel@gmail.com>
2023-05-17 19:16:06 -0700

    add README and passing Order tests

---
+++ PierresBakery.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,84 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PierresBakery.Models;
+using System;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Sourdough Loaf";
+
+      //Act
+      Order newOrder = new Order(description);
+      string result = newOrder.Description;
+
+      //Assert
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Sourdough Loaf";
+      Order newOrder = new Order(description);
+
+      //Act
+      string updatedDescription = "Rye Loaf";
+      newOrder.Description = updatedDescription;
+      string result = newOrder.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      // Arrange
+      List<Order> newList = new List<Order> { };
+
+      // Act
+      List<Order> result = Order.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      //Arrange
+      string description01 = "White Loaf";
+      string description02 = "Sourdough Loaf";
+      Order newOrder1 = new Order(description01);
+      Order newOrder2 = new Order(description02);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+
+      //Act
+      List<Order> result = Order.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## What Is This?
+
+This is a C# project I completed while attending Epicoodus. It's an ASP.NET Core MVC web application that keeps track of a baker's vendors and vendor orders.
+
+## How To Run This Project
+
+1. Clone this repo.
+2. Open your shell (e.g., Terminal or GitBash) and navigate to this project's production directory called "PierresBakery".
+3. Run `dotnet watch run` in the command line to start the project in development mode with a watcher.
+4. Open the browser to _https://localhost:5001_. If you cannot access localhost:5001 it is likely because you have not configured a .NET developer security certificate for HTTPS.
